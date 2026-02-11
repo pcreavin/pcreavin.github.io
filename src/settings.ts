5467ff0fcacc6e85be8bfc84218f1c01d99141d0
Paul Creavin <pcreavin@stanford.edu>
2026-02-11 15:39:38 -0800

    Update homepage and projects

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 export const profile = {
-	fullName: 'Your Name',
+	fullName: 'Paul Creavin',
 	title: '',
-	institute: '',
+	institute: 'Stanford University',
 	author_name: '', // Author name to be highlighted in the papers section
 	research_areas: [
 		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
